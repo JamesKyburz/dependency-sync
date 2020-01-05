7dbe0a3ec0d69c0f48d6101d3c5f03d78f515d4a
James Kyburz <james.kyburz@gmail.com>
2020-01-05 14:18:56 +0100

    remove config lambda

--- src/lib/config.js
+++ src/lib/config.js
@@ -6,13 +6,12 @@ var config = Object.assign(
     args: [],
     keep: [],
     ignore: [],
-    yarn: false,
-    lambda: false
+    yarn: false
   },
   pkgConfig('dependency-sync', { root: false, cwd: process.cwd() })
 )
 
-config.ignore = config.ignore.map((x) => path.relative(process.cwd(), x))
+config.ignore = config.ignore.map(x => path.relative(process.cwd(), x))
 
 config.keep.push('dependency-sync')
 config.args.push.apply(config.args, process.argv.slice(2))
@@ -25,11 +24,7 @@ if (config.args.indexOf('--yarn') !== -1 && !config.yarn) {
   config.yarn = { args: [] }
 }
 
-if (config.args.indexOf('--lambda') !== -1) {
-  config.lambda = true
-}
-
-if (!config.args.filter((x) => !x.match(/^--/)).length) {
+if (!config.args.filter(x => !x.match(/^--/)).length) {
   config.args.unshift('.')
 }
 
